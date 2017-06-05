9d64f4589364c6fc3b134b723a9214780a260f57
socoolbear <socoolbear@gmail.com>
2017-06-06 00:18:22 +0900

    exists sync log dir

--- core/logger.js
+++ core/logger.js
@@ -4,6 +4,12 @@ const fs = require('fs'),
     winston = require('winston');
 const today = new Date();
 const datestr = today.getFullYear() + '' + ("0" + (today.getMonth() + 1)).slice(-2);
+
+let dir = __dirname + '/../logs';
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
 const filename = path.join(__dirname, '../logs/' + datestr + '.log');
 let logger = new (winston.Logger)({
   transports: [
